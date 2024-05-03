91b56a80032679091c48fcac96b615bb2a26c471
yuraiz <7516890@gmail.com>
2024-05-03 17:56:36 +0300

    feat(gradient_bg): Use software rendering for GradientBg
    GskGLShaderNode is a broken thing, so it isn't supported with new renderers.
    
    The code for the software gradient rendering is adapted from the code written by Mikhail Filimonov for TelegramSwift.
    
    The gradient is currently rendered at very low resolution (32x32) and upscaled on GPU, probably it's better to use higher resolution, but for me looks good enough.

--- demo/src/window/gradient_bg/mod.rs
+++ demo/src/window/gradient_bg/mod.rs
@@ -1,15 +1,17 @@
-use adw::prelude::*;
 use adw::subclass::prelude::*;
-use glib::clone;
 use gtk::gdk;
 use gtk::glib;
 
 mod imp {
+    use gtk::glib::clone;
+
     use super::*;
 
     #[derive(Debug, Default, gtk::CompositeTemplate)]
     #[template(file = "src/window/gradient_bg/gradient_bg.blp")]
     pub struct GradientBgPage {
+        #[template_child]
+        pub(super) drop_target: TemplateChild<gtk::DropTarget>,
         #[template_child]
         pub(super) gradient_bg: TemplateChild<ori::GradientBg>,
     }
@@ -33,6 +35,30 @@ mod imp {
     impl ObjectImpl for GradientBgPage {
         fn constructed(&self) {
             self.parent_constructed();
+
+            let gradient_bg = self.gradient_bg.to_owned();
+
+            let style_manager = adw::StyleManager::default();
+            gradient_bg.set_dark(style_manager.is_dark());
+            gradient_bg.set_theme_colors(hard_coded_theme_colors(style_manager.is_dark()));
+
+            style_manager.connect_dark_notify(clone!(@weak gradient_bg => move |style_manager| {
+                gradient_bg.set_dark(style_manager.is_dark());
+                gradient_bg.set_theme_colors(hard_coded_theme_colors(style_manager.is_dark()))
+            }));
+
+            self.drop_target.connect_drop(
+                clone!(@weak gradient_bg => @default-return false, move
+                    |_, value, _, _ | {
+                        let Ok(file_list) = value.get::<gdk::FileList>() else { return false; };
+
+                        let pattern_texture = file_list.files().iter().filter_map(|file| gdk::Texture::from_file(file).ok()).next();
+                        
+                        gradient_bg.set_pattern(pattern_texture);
+                        true
+                    }
+                ),
+            );
         }
     }
 
@@ -51,4 +77,25 @@ mod imp {
 glib::wrapper! {
     pub struct GradientBgPage(ObjectSubclass<imp::GradientBgPage>)
         @extends adw::Bin, gtk::Widget;
+}
+
+fn hard_coded_theme_colors(dark: bool) -> Vec<gtk::graphene::Vec3> {
+    let colors = if dark {
+        vec![0xd6932e, 0xbc40db, 0x4280d7, 0x614ed5]
+    } else {
+        vec![0x94dae9, 0x9aeddb, 0x94c3f6, 0xac96f7]
+    };
+
+    let colors = colors
+        .into_iter()
+        .map(|int_color| {
+            let r = (int_color >> 16) & 0xFF;
+            let g = (int_color >> 8) & 0xFF;
+            let b = int_color & 0xFF;
+
+            gtk::graphene::Vec3::new(r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0)
+        })
+        .collect();
+
+    colors
 }
--- origami/src/gradient_bg/mod.rs
+++ origami/src/gradient_bg/mod.rs
@@ -1,3 +1,5 @@
+mod software_gradient;
+
 use std::cell::Cell;
 use std::cell::OnceCell;
 use std::cell::RefCell;
@@ -6,59 +8,16 @@ use adw::prelude::*;
 use adw::subclass::prelude::*;
 use glib::clone;
 use gtk::gdk;
-use gtk::gio;
 use gtk::glib;
 use gtk::graphene;
-use gtk::gsk;
-
-const GRADIENT_SHADER: &[u8] = r#"
-// That shader was taken from Telegram for android source
-// https://github.com/DrKLO/Telegram/commit/2112affb2e4941334f8fbc3944385806b3c4e3d6#diff-dfdd1e8c4691747fd30199b7a2f5041a126b23e1450b29afe441eb0ebed01c68
-
-precision highp float;
-
-uniform vec3 color1;
-uniform vec3 color2;
-uniform vec3 color3;
-uniform vec3 color4;
-uniform vec2 p1;
-uniform vec2 p2;
-uniform vec2 p3;
-uniform vec2 p4;
-
-void mainImage(out vec4 fragColor,
-               in vec2 fragCoord,
-               in vec2 resolution,
-               in vec2 uv) {
-    uv.y = 1.0 - uv.y;
-
-    float dp1 = distance(uv, p1);
-    float dp2 = distance(uv, p2);
-    float dp3 = distance(uv, p3);
-    float dp4 = distance(uv, p4);
-    float minD = min(dp1, min(dp2, min(dp3, dp4)));
-    float p = 5.0;
-    dp1 = pow(1.0 - (dp1 - minD), p);
-    dp2 = pow(1.0 - (dp2 - minD), p);
-    dp3 = pow(1.0 - (dp3 - minD), p);
-    dp4 = pow(1.0 - (dp4 - minD), p);
-    float sumDp = dp1 + dp2 + dp3 + dp4;
-
-    vec3 color = (color1 * dp1 + color2 * dp2 + color3 * dp3 + color4 * dp4) / sumDp;
-    fragColor = vec4(color, 1.0);
-}
-"#
-.as_bytes();
 
 mod imp {
     use super::*;
 
     #[derive(Default)]
     pub struct GradientBg {
-        pub(super) gradient_texture: RefCell<Option<gdk::Texture>>,
-        pub(super) last_size: Cell<(f32, f32)>,
+        pub(super) gradient_texture: RefCell<Option<gdk::MemoryTexture>>,
 
-        pub(super) shader: RefCell<Option<gsk::GLShader>>,
         pub(super) pattern: RefCell<Option<gdk::Texture>>,
 
         pub(super) animation: OnceCell<adw::Animation>,
@@ -66,7 +25,6 @@ mod imp {
         pub(super) phase: Cell<u32>,
 
         pub(super) dark: Cell<bool>,
-
         pub(super) colors: RefCell<Vec<graphene::Vec3>>,
     }
 
@@ -84,12 +42,6 @@ mod imp {
             let obj = self.obj();
 
             let style_manager = adw::StyleManager::default();
-            obj.set_theme_colors(hard_coded_theme_colors(style_manager.is_dark()));
-
-            style_manager.connect_dark_notify(clone!(@weak obj => move |style_manager| {
-                obj.imp().dark.set(style_manager.is_dark());
-                obj.set_theme_colors(hard_coded_theme_colors(style_manager.is_dark()))
-            }));
 
             if style_manager.is_high_contrast() {
                 obj.add_css_class("fallback");
@@ -98,8 +50,6 @@ mod imp {
             style_manager.connect_high_contrast_notify(clone!(@weak obj => move |style_manager| {
                 if style_manager.is_high_contrast() {
                     obj.add_css_class("fallback");
-                } else if obj.imp().shader.borrow().is_some() {
-                    obj.remove_css_class("fallback");
                 }
             }));
 
@@ -131,11 +81,6 @@ mod imp {
     }
 
     impl WidgetImpl for GradientBg {
-        fn realize(&self) {
-            self.parent_realize();
-            self.obj().ensure_shader();
-        }
-
         fn snapshot(&self, snapshot: &gtk::Snapshot) {
             let widget = self.obj();
 
@@ -156,9 +101,7 @@ mod imp {
 
             let bounds = graphene::Rect::new(0.0, 0.0, width, height);
 
-            let size_changed = self.last_size.replace((width, height)) != (width, height);
-
-            self.snapshot_gradient(snapshot, &bounds, size_changed);
+            self.snapshot_gradient(snapshot, &bounds);
 
             self.snapshot_pattern(snapshot, &bounds);
         }
@@ -167,26 +110,45 @@ mod imp {
     impl BinImpl for GradientBg {}
 
     impl GradientBg {
-        fn snapshot_gradient(
-            &self,
-            snapshot: &gtk::Snapshot,
-            bounds: &graphene::Rect,
-            size_changed: bool,
-        ) {
-            if self.progress.get() == 0.0 {
-                let texture = match self.gradient_texture.take() {
-                    Some(texture) if !size_changed => texture,
-                    _ => {
-                        let renderer = self.obj().native().unwrap().renderer().unwrap();
-                        renderer.render_texture(self.gradient_shader_node(bounds), Some(bounds))
-                    }
-                };
+        fn snapshot_gradient(&self, snapshot: &gtk::Snapshot, bounds: &graphene::Rect) {
+            let progress = self.progress.get();
+            let phase = self.phase.get() as usize;
 
-                snapshot.append_texture(&texture, bounds);
-                self.gradient_texture.replace(Some(texture));
+            let cached_texture = (*self.gradient_texture.borrow()).clone();
+
+            let texture = if let Some(texture) = cached_texture {
+                texture
             } else {
-                snapshot.append_node(self.gradient_shader_node(bounds));
-            }
+                let colors = &*self.colors.borrow();
+
+                let positions = Self::calculate_positions(progress, phase);
+
+                // Even with 4x4 the upscaled result looks good,
+                // but I think that 32x32 is better
+                let (width, height) = (32, 32);
+
+                let raw_texture = software_gradient::generate_gradient(
+                    software_gradient::Size { width, height },
+                    colors,
+                    &positions,
+                );
+
+                let texture = gdk::MemoryTexture::new(
+                    width as i32,
+                    height as i32,
+                    gdk::MemoryFormat::B8g8r8a8,
+                    &glib::Bytes::from_owned(raw_texture),
+                    4 * width as usize,
+                );
+
+                if progress == 0.0 {
+                    self.gradient_texture.replace(Some(texture.clone()));
+                }
+
+                texture
+            };
+
+            snapshot.append_texture(&texture, &bounds);
         }
 
         fn snapshot_pattern(&self, snapshot: &gtk::Snapshot, bounds: &graphene::Rect) {
@@ -226,65 +188,18 @@ mod imp {
             }
         }
 
-        fn gradient_shader_node(&self, bounds: &graphene::Rect) -> gsk::GLShaderNode {
-            let Some(gradient_shader) = &*self.shader.borrow() else {
-                unreachable!()
-            };
+        fn calculate_positions(progress: f32, phase: usize) -> Vec<software_gradient::Point> {
+            let mut current = software_gradient::gather_positions(phase);
+            let next = software_gradient::gather_positions(phase + 1);
 
-            let args_builder = gsk::ShaderArgsBuilder::new(gradient_shader, None);
-
-            let progress = self.progress.get();
-            let phase = self.phase.get() as usize;
-
-            let colors = self.colors.borrow();
-
-            let &[c1, c2, c3, c4] = &colors[..] else {
-                unimplemented!("Unexpected color count");
-            };
-
-            args_builder.set_vec3(0, &c1);
-            args_builder.set_vec3(1, &c2);
-            args_builder.set_vec3(2, &c3);
-            args_builder.set_vec3(3, &c4);
-
-            let [p1, p2, p3, p4] = Self::calculate_positions(progress, phase);
-            args_builder.set_vec2(4, &p1);
-            args_builder.set_vec2(5, &p2);
-            args_builder.set_vec2(6, &p3);
-            args_builder.set_vec2(7, &p4);
-
-            gsk::GLShaderNode::new(gradient_shader, bounds, &args_builder.to_args(), &[])
-        }
-
-        fn calculate_positions(progress: f32, phase: usize) -> [graphene::Vec2; 4] {
-            static POSITIONS: [(f32, f32); 8] = [
-                (0.80, 0.10),
-                (0.60, 0.20),
-                (0.35, 0.25),
-                (0.25, 0.60),
-                (0.20, 0.90),
-                (0.40, 0.80),
-                (0.65, 0.75),
-                (0.75, 0.40),
-            ];
-
-            let mut points = [graphene::Vec2::new(0.0, 0.0); 4];
-
-            for i in 0..4 {
-                let start = POSITIONS[(i * 2 + phase) % 8];
-                let end = POSITIONS[(i * 2 + phase + 1) % 8];
-
-                fn interpolate(start: f32, end: f32, value: f32) -> f32 {
-                    start + ((end - start) * value)
-                }
-
-                let x = interpolate(start.0, end.0, progress);
-                let y = interpolate(start.1, end.1, progress);
-
-                points[i] = graphene::Vec2::new(x, y);
+            if progress > 0.0 {
+                current
+                    .iter_mut()
+                    .zip(next)
+                    .for_each(|(current, next)| *current = current.interpolate(next, progress))
             }
 
-            points
+            current
         }
     }
 }
@@ -301,6 +216,10 @@ impl GradientBg {
 
     pub fn set_pattern(&self, pattern: Option<gdk::Texture>) {
         self.imp().pattern.replace(pattern);
+    }
+
+    pub fn set_dark(&self, dark: bool) {
+        self.imp().dark.set(dark);
     }
 
     pub fn set_theme_colors(&self, colors: Vec<graphene::Vec3>) {
@@ -320,51 +239,10 @@ impl GradientBg {
             animation.play()
         }
     }
-
-    fn ensure_shader(&self) {
-        let imp = self.imp();
-        if imp.shader.borrow().is_none() {
-            let renderer = self.native().unwrap().renderer().unwrap();
-
-            let shader = gsk::GLShader::from_bytes(&GRADIENT_SHADER.into());
-            match shader.compile(&renderer) {
-                Err(e) => {
-                    if !e.matches(gio::IOErrorEnum::NotSupported) {
-                        log::error!("can't compile shader for gradient background {e}");
-                    }
-                    self.add_css_class("fallback");
-                }
-                Ok(_) => {
-                    imp.shader.replace(Some(shader));
-                }
-            }
-        };
-    }
 }
 
 impl Default for GradientBg {
     fn default() -> Self {
         Self::new()
     }
-}
-
-fn hard_coded_theme_colors(dark: bool) -> Vec<graphene::Vec3> {
-    let colors = if dark {
-        vec![0xd6932e, 0xbc40db, 0x4280d7, 0x614ed5]
-    } else {
-        vec![0x94dae9, 0x9aeddb, 0x94c3f6, 0xac96f7]
-    };
-
-    let colors = colors
-        .into_iter()
-        .map(|int_color| {
-            let r = (int_color >> 16) & 0xFF;
-            let g = (int_color >> 8) & 0xFF;
-            let b = int_color & 0xFF;
-
-            graphene::Vec3::new(r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0)
-        })
-        .collect();
-
-    colors
 }
